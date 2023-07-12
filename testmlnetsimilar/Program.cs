9d967bff4bf1d32e0974dfa1a612b407299e165c
hieunv1 <hieunv1@ctin.vn>
2023-07-12 14:24:21 +0700

    .

---
+++ testmlnetsimilar/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.Text;
+using testmlnetsimilar;
+
+
+// Tạo một instance của MLContext
+var mlContext = new MLContext();
+
+// Dữ liệu câu mẫu
+string sentence1 = "Chuyên nghiệp, Hòa đồng, Tỉ mỉ, Tốt bụng, Trung thực";
+string sentence2 = "Tỉ bụng , Chăm đồng , Chỉnh chu, Dễ gần, Nghiêm túc";
+
+// Tạo một IDataView chứa dữ liệu mẫu
+var dataView = mlContext.Data.LoadFromEnumerable(new[] { new SentenceData { Sentence = sentence1 }, new SentenceData { Sentence = sentence2 } });
+
+// Định nghĩa TextFeaturizingEstimator
+var textFeaturizer = mlContext.Transforms.Text.FeaturizeText("Features", new TextFeaturizingEstimator.Options
+{
+    OutputTokensColumnName = "Tokens"
+}, "Sentence");
+
+// Fit và transform dữ liệu
+var transformedData = textFeaturizer.Fit(dataView).Transform(dataView);
+
+// Lấy giá trị đặc trưng của các câu
+var features = mlContext.Data.CreateEnumerable<FeatureData>(transformedData, reuseRowObject: false);
+
+// Lấy đặc trưng của câu 1 và câu 2
+var feature1 = features.ElementAt(0).Features;
+var feature2 = features.ElementAt(1).Features;
+
+// Tính toán độ tương đồng sử dụng Cosine Similarity
+var cosineSimilarity = Test.CalculateCosineSimilarity(feature1, feature2);
+
+Console.WriteLine($"Cosine Similarity: {cosineSimilarity}");
+
+public class SentenceData
+{
+    public string Sentence { get; set; }
+}
+
+public class FeatureData
+{
+    public float[] Features { get; set; }
+}
+
---
+++ testmlnetsimilar/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testmlnetsimilar
+{
+    public class Test
+    {
+        public static double CalculateCosineSimilarity(float[] vector1, float[] vector2)
+        {
+            double dotProduct = 0.0;
+            double magnitude1 = 0.0;
+            double magnitude2 = 0.0;
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                dotProduct += vector1[i] * vector2[i];
+                magnitude1 += Math.Pow(vector1[i], 2);
+                magnitude2 += Math.Pow(vector2[i], 2);
+            }
+            magnitude1 = Math.Sqrt(magnitude1);
+            magnitude2 = Math.Sqrt(magnitude2);
+            if (magnitude1 == 0.0 || magnitude2 == 0.0)
+                return 0.0;
+            else
+                return dotProduct / (magnitude1 * magnitude2);
+        }
+    }
+}
